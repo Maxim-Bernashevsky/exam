398ecca777acfe3232ae1050cdb185bd01784f68
chrstnv <christinavolosyuk@gmail.com>
2017-05-28 00:18:36 +0300

    get hall

--- films/service.php
+++ films/service.php
@@ -16,6 +16,9 @@ if(isset ($_POST['type']) && isset($_POST['data'])) {
         case 'getSeance':
             getSeance($data);
             break;
+        case 'getHall':
+            getHall($data);
+            break;
         default:
             sendErrorMessage("Not found");
             break;
@@ -129,23 +132,25 @@ function getSeance($data) {
         $data = json_decode($data, true);
         $id = $data['id'];
         $sql = 'SELECT 
-                DATE_FORMAT(`seance`.`datetime`,"%d/%m/%Y") AS `seance_date`,
-                `seance`.`ID` AS `seance_id`,
-                `movies`.`name` AS `movie_name`,
-                `genre`.`name` AS `genre_name`,
-                TIME(`seance`.`datetime`) AS `seance_time`,
-                `movies`.`desc` AS `desc`,
-                `cinema`.`name` AS `cinema_name`,
-                `hall`.`name` AS `hall_name`,
-                `seance`.`price` AS `seance_price`,
-                CONCAT(`directed`.`first_name`,\' \', `last_name`) AS `directed_by`	
-                FROM `seance`
-                LEFT JOIN `movies` ON `seance`.`ID_movie` = `movies`.`id`
-                LEFT JOIN `directed` ON `movies`.`id_directed` = `directed`.`ID`
-                LEFT JOIN `genre` ON `movies`.`ID_genre` = `genre`.`ID`
-                LEFT JOIN `hall` ON `seance`.`ID_hall` = `hall`.`ID`
-                LEFT JOIN `cinema` ON `hall`.`ID_cinema` = `cinema`.`ID`
-                WHERE `seance`.`ID` = '.$id;
+                    DATE_FORMAT(`seance`.`datetime`,"%d/%m/%Y") AS `seance_date`,
+                    `seance`.`ID` AS `seance_id`,
+                    `movies`.`name` AS `movie_name`,
+                    `movies`.`ID` AS `movie_id`,
+                    `genre`.`name` AS `genre_name`,
+                    TIME(`seance`.`datetime`) AS `seance_time`,
+                    `movies`.`desc` AS `desc`,
+                    `cinema`.`name` AS `cinema_name`,
+                    `hall`.`name` AS `hall_name`,
+                    `hall`.`ID` AS `hall_id`,
+                    `seance`.`price` AS `seance_price`,
+                    CONCAT(`directed`.`first_name`,\' \', `last_name`) AS `directed_by`	
+                    FROM `seance`
+                    LEFT JOIN `movies` ON `seance`.`ID_movie` = `movies`.`id`
+                    LEFT JOIN `directed` ON `movies`.`id_directed` = `directed`.`ID`
+                    LEFT JOIN `genre` ON `movies`.`ID_genre` = `genre`.`ID`
+                    LEFT JOIN `hall` ON `seance`.`ID_hall` = `hall`.`ID`
+                    LEFT JOIN `cinema` ON `hall`.`ID_cinema` = `cinema`.`ID`
+                    WHERE `seance`.`ID` = '.$id;
         $queryFilm = mysqli_query($db, $sql);
         if (mysqli_num_rows($queryFilm) > 0) {
             $film = mysqli_fetch_assoc($queryFilm);
@@ -153,16 +158,61 @@ function getSeance($data) {
                         CONCAT(`actor`.`first_name`,\' \', `actor`.`last_name`) AS `actor_name`
                         FROM `actor_list`
                         LEFT JOIN `actor` ON `actor`.`id` = `actor_list`.`ID_actor`
-                        WHERE `actor_list`.`ID_movis` ='.$id;
+                        WHERE `actor_list`.`ID_movis` ='.$film['movie_id'];
             $queryActors = mysqli_query($db, $sql);
             if (mysqli_num_rows($queryActors) > 0) {
                 $actors = mysqli_fetch_all($queryActors);
                 foreach ($actors as $key => &$value) {
                     $actors[$key] = $value[0];
                 }
+                unset($value);
                 $film ['actors'] = implode(",", $actors);
             }
             $response = json_encode($film, JSON_UNESCAPED_UNICODE);
+            echo $response;
+        } else sendErrorMessage('Данные не получены');
+    } else sendErrorMessage('Данные не получены');
+}
+
+function getHall($data) {
+    $db = connect();
+    if($db){
+        $data = json_decode($data, true);
+        $id_seance = $data['id_seance'];
+        $sql = 'SELECT
+                  `seance`.`ID` AS `seance_id`,
+                  `seance`.`ID_hall` AS `hall_id`,
+                  `place`.`row`,
+                  `place`.`number`,
+                  (SELECT
+                    `ID_status` FROM `ticket`
+                    WHERE `ticket`.`ID_seance` = `seance`.`ID` AND
+                    `ticket`.`row` = `place`.`row` AND
+                    `ticket`.`number` = `place`.`number`) AS `place_status`
+                    FROM `seance`
+                    LEFT JOIN `place` ON `seance`.`ID_hall` = `place`.`ID_hall`
+                    WHERE `seance`.`ID`='.$id_seance;
+
+        $query = mysqli_query($db, $sql);
+        if (mysqli_num_rows($query) > 0) {
+            $hall_data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+            $rows = array();
+            foreach ($hall_data as $value) {
+                  $rows [] = $value['row'];
+            }  $rows = array_flip(array_unique($rows));
+            $hall['rows'] = $rows;
+            foreach ($rows as $key => $row) {
+                $places = array();
+                foreach ($hall_data as $value) {
+                    if($value['row']==$key){
+                        $places [] = array(
+                                        "number" => $value['number'],
+                                        "status" => $value['place_status'] ? $value['place_status'] : 0);
+                    }
+                }
+                $hall['rows'][$key] = array('places' => $places);
+            }
+            $response = json_encode($hall, JSON_UNESCAPED_UNICODE);
             echo $response;
         } else sendErrorMessage('Данные не получены');
     } else sendErrorMessage('Данные не получены');
